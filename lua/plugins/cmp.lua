512636451ab366275e6fd9d8c5d67aa508777acd
Ken Andrew Taylor <kentaylorappdev@gmail.com>
2024-09-22 14:25:12 -0400

    upd

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,19 +6,48 @@ return {
       -- codeium
       {
         "Exafunction/codeium.nvim",
-        enabled = false,
+        enabled = true,
         cmd = "Codeium",
         build = ":Codeium Auth",
         opts = {},
       },
       {
         "supermaven-inc/supermaven-nvim",
-        enabled = false,
-        opts = {},
+        enabled = true,
+        opts = {
+          disable_inline_completion = true, -- disables inline completion for use with cmp
+          disable_keymaps = true,
+        },
       },
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
+      local cmp = require("cmp")
+
+      -- Disable automatic completion
+      opts.completion = opts.completion or {}
+      opts.completion.autocomplete = false
+
+      -- Configure Tab to trigger completion
+      opts.mapping = opts.mapping or {}
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          cmp.complete()
+        end
+      end, { "i", "s" })
+
+      -- Configure Shift+Tab to select previous item
+      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      -- Your existing source configurations
       table.insert(opts.sources, 1, {
         name = "codeium",
         group_index = 1,
